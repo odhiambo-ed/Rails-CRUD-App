2880e4dde8debdd7744af4e6d48590c37ff3ccc0
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-20 22:28:57 +0300

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
   get 'static_pages/dashboard'
   devise_for :users
   resources :users
   resources :friends
 
-  root 'friends#index'
+  root 'static_pages#landing_page'
+  # root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
