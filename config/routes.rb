df53717ac5ecb6903c7884210e58c4556ed6de52
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-07-24 13:29:35 +0300

    Edit Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :users
   resources :friends
 
-  root 'static_pages#landing_page'
-  # root 'friends#index'
+  # root 'static_pages#landing_page'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
